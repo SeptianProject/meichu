316d38529f8f76e038d1f1cdc8bc445bd0ac474b
Septian Asropik <septianuwoe75@gmail.com>
2024-11-26 08:37:44 +0700

    fix: assets images

--- src/assets/assets.tsx
+++ src/assets/assets.tsx
@@ -30,11 +30,6 @@ import Product1 from './images/product1.jpg';
 import Product2 from './images/product2.jpg';
 import Product3 from './images/product3.jpg';
 import Product4 from './images/product4.jpg';
-// import Product5 from './images/product5.jpg';
-// import Product6 from './images/product6.jpg';
-// import Product7 from './images/product7.jpg';
-// import Product8 from './images/product8.jpg';
-// import Product9 from './images/product9.jpg';
 
 
 export const assetsImage = {
